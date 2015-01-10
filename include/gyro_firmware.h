dcf87d0a0e40e8c47ca9814200b9000eec7a8b9c
Banti Gheneti <banti@gheneti.com>
2015-01-10 00:37:36 -0500

    gyro header changes

--- include/gyro_firmware.h
+++ include/gyro_firmware.h
@@ -1,4 +1,4 @@
-#ifndef INCLUDE_GYRO_FIRMWARE_H_
+s#ifndef INCLUDE_GYRO_FIRMWARE_H_
 #define INCLUDE_GYRO_FIRMWARE_H_
 
 #include "mraa.hpp"
@@ -9,16 +9,23 @@ namespace{
             double angle;
 	    double angularVelocity;
         public:
-	    Gyro(int dirPin);
+	    //Creates a new gyro object that has a slave select pin  
+	    Gyro(int slaveSelectPin);
 	    
+	    //start a thread for polling gyro data
 	    void startPoll();
-
+	    
+	    //stops polling gyro data
 	    void stopPoll();
 	    
+	    //returns the current angle in degrees
 	    void getAngle();
-
+	    
+	    //returns the current angular velocity in degrees/second
 	    double getAngularV();
 
+	    //sets the current angle to zero
 	    double zeroAngle();
     };
 }
+#endif
