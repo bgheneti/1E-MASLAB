9cb23e75853d10260f3457f5f0a9105047eeabf4
Banti Gheneti <banti@gheneti.com>
2015-01-11 23:29:48 -0500

    working gyro firmware

--- include/gyro_firmware.h
+++ include/gyro_firmware.h
@@ -1,13 +1,20 @@
-s#ifndef INCLUDE_GYRO_FIRMWARE_H_
+#ifndef INCLUDE_GYRO_FIRMWARE_H_
 #define INCLUDE_GYRO_FIRMWARE_H_
 
 #include "mraa.hpp"
-
-namespace{
-    class GYRO {
+#include <thread>
+namespace firmware{
+    class Gyro {
         private:
             double angle;
 	    double angularVelocity;
+	    mraa::Spi spi;
+            mraa::Gpio slaveSelect;
+            int slaveSelectPin;
+	    int running;
+	    std::thread runner;
+            int zero;
+	    void poll();
         public:
 	    //Creates a new gyro object that has a slave select pin  
 	    Gyro(int slaveSelectPin);
@@ -19,13 +26,14 @@ namespace{
 	    void stopPoll();
 	    
 	    //returns the current angle in degrees
-	    void getAngle();
+	    double getAngle();
 	    
 	    //returns the current angular velocity in degrees/second
 	    double getAngularV();
 
 	    //sets the current angle to zero
-	    double zeroAngle();
+	    void zeroAngle();
     };
 }
 #endif
+
