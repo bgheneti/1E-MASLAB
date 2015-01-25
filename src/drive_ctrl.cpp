92dd08ec61d12d2d613335f5c650a029bf66085b
MASLAB 1E Edison <maslab1e@mit.edu>
2015-01-25 02:47:40 +0000

    Improved drive code and tuned encoders

--- src/drive_ctrl.cpp
+++ src/drive_ctrl.cpp
@@ -43,8 +43,6 @@ namespace drive {
         } else {
             bias = 0;
         }
-        leftMotor.setSpeed(bias);
-        rightMotor.setSpeed(bias);
         while(leftEncoder.getDistance() < std::abs(distance) ||
               (distance < .0001 && std::abs(heading - gyro.getAngle()) > 1.0 )) { 
             double diff = heading - gyro.getAngle();
@@ -56,20 +54,42 @@ namespace drive {
             integral += diff * dT;
             double derivative = gyro.getAngularV();
             power = P*diff + I*integral + D*derivative;
-            double leftMotorSpeed = bias - power;
+            double leftMotorSpeed = bias - .05 - power;
             double rightMotorSpeed = bias + power;
             if(std::abs(leftMotorSpeed) > .5) {
                 leftMotorSpeed = leftMotorSpeed < 0 ? -.5 : .5;
             }
             if(std::abs(rightMotorSpeed) > .5) {
                 rightMotorSpeed = rightMotorSpeed < 0 ? -.5: .5;
+		
             }
+
             leftMotor.setSpeed(leftMotorSpeed);
             rightMotor.setSpeed(rightMotorSpeed);
             usleep(50000);
         }
-     	leftMotor.setSpeed(0.0);
-   	    rightMotor.setSpeed(0.0);
+	double MAX_ACCEL = .005;
+	while(leftMotor.getSpeed() > .0001 || rightMotor.getSpeed() > .0001) {
+            gettimeofday(&currentTime, NULL);
+            double newCurrentMS = ((double)currentTime.tv_sec)*1000.0 +
+                                  ((double)currentTime.tv_usec)/1000.0;
+            double dT = newCurrentMS - currentMS;
+	    currentMS = newCurrentMS;
+
+            if(std::abs(rightMotor.getSpeed())/dT > MAX_ACCEL) {
+		double sign = rightMotor.getSpeed() > 0 ? -1.0 : 1.0;
+		rightMotor.setSpeed(MAX_ACCEL*dT*sign + rightMotor.getSpeed());
+	    } else {
+		rightMotor.setSpeed(0.0);
+	    }
+	    if(std::abs(leftMotor.getSpeed())/dT > MAX_ACCEL) {
+		double sign = leftMotor.getSpeed() > 0 ? -1.0 : 1.0;
+		leftMotor.setSpeed(MAX_ACCEL*dT*sign + leftMotor.getSpeed());
+	    } else {
+		leftMotor.setSpeed(0.0);
+	    }
+	    usleep(1000000);
+	}
     }
 
     void DriveTrain::straightForDistance(double distance) {
--- src/main.cpp
+++ src/main.cpp
@@ -51,8 +51,8 @@ int main() {
     drive::DriveTrain dt(motorL, motorR, encoderL, encoderR, gyro);
     usleep(500000);
     std::cout << "running" << std::endl;
-    dt.straightForDistance(.2);
-    dt.turnForDegrees(50.0);
+    dt.straightForDistance(1.0);
+ //   dt.turnForDegrees(50.0);
     std::cout << "running" << std::endl;
 
     encoderL.stopPolling();
