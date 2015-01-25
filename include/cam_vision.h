f9a482841d34da3eac51bf22e7163e35611d934a
Banti Gheneti <banti@gheneti.com>
2015-01-25 00:31:18 -0500

    vision

--- include/cam_vision.h
+++ include/cam_vision.h
@@ -23,11 +23,13 @@ namespace vision{
 	    void addMat(Mat& a, Mat& b);
 	    void findCubes(Mat& inFrame);
 	    void processFrame(Mat& inFrame, Mat& outFrame);
-	    std::vector<Block*> blocks;
-        public:	    
+	    std::vector<double> expandPoint(int x, int y, Mat& inFrame);
+	    std::vector<double> findPoint(Mat& inFrame);
+	    std::vector<Block> blocks;
+        public:	  
 	    void startPoll();
 	    void stopPoll();
-	    std::vector<Block*> getBlocks();
+	    std::vector<Block> getBlocks();
     };
 }
 #endif
--- src/cam_vision.cpp
+++ src/cam_vision.cpp
@@ -5,8 +5,8 @@
 #include <stdio.h>
 #include "opencv2/opencv.hpp"
 #include "cmath"
-#include <complex>   
-
+#include <complex>
+#include <queue>  
 using namespace cv;
 using namespace vision;
 namespace vision{
@@ -47,7 +47,70 @@ namespace vision{
     outFrame=inFrame;
   }
 
+  //finds closest point that corresponds to a potential block in the inFrame
+  std::vector<double> Cam::expandPoint(int x, int y, Mat& inFrame){
+    std::vector<double> vals(3,0);
+    vals[0]=0;
+    vals[1]=0;
+    vals[2]=inFrame.rows;
+    std::queue<vector<int>> points;
+    std::vector<int> point;
+    point.push_back(x);
+    point.push_back(y);
+    points.push(point);
+    while(points.size()>0){
+      std::vector<int> currentPoint=points.front();
+      points.pop();
+      int x=currentPoint[0];
+      int y=currentPoint[1];
+      //std::cout<<x<<","<<y<<std::endl;
+      vals[0]+=1;
+      if(vals[2]<currentPoint[1]){
+	vals[1]=currentPoint[0];
+	vals[2]=currentPoint[1];
+      }
+      for(int i=std::max(x-1,0);i<std::min(x+2,inFrame.cols);i++){
+         for(int j=std::max(y-1,0);j<std::min(y+2,inFrame.rows);j++){
+	   if((i!=x) && (j!=y) && (inFrame.at<Vec3b>(i,j)[0]==255)){
+	     inFrame.at<Vec3b>(i,j)[0]=254;
+	     std::vector<int> newPoint;
+	     newPoint.push_back(i);
+	     newPoint.push_back(j);
+	     points.push(newPoint);
+	   }
+	 }
+      }
+      
+    }
+    return vals;
+  }
+  //finds closest block if there is one in inFrame
+  vector<double> Cam::findPoint(Mat& inFrame){
+    //std::cout<<"find"<<std::endl;
+    std::vector<double> point(2,-1);
+    int width=inFrame.cols;
+    int height=inFrame.rows;
+    for(int i=0; i<width;i++){
+      for(int j=0;j<height;j++){
+	if(inFrame.at<Vec3b>(i,j)[0]==255){
+	  //std::cout<<"found"<<std::endl;
+	  inFrame.at<Vec3b>(i,j)[0]=254;
+	  std::vector<double>val=expandPoint(i,j,inFrame);
+	  //std::cout<<"setting found"<<std::endl;
+	  if(val[0]>40 && val[2]>point[1]){
+	    point[0]=val[1];
+	    point[1]=val[2];
+	  }
+	}
+      }
+    }
+    return point;
+  }
+
+
+  //sets entries in vector blocks to the closest red block and the closest green block
   void Cam::findCubes(Mat& inFrame){
+    std::cout<<"find cubes"<<std::endl;
     Mat_<Vec3b> greenFrame(240, 320, Vec3b(0,0,0));
     Mat_<Vec3b> redFrame(240, 320, Vec3b(0,0,0));
     //inFrame=redFrame;
@@ -57,9 +120,13 @@ namespace vision{
       uchar* greenFramePos = greenFrame.ptr(row);    
       for(int col = 0; col < inFrame.cols; col++) {
 	if(*(inFramePos+1)==255){
+	  *(greenFramePos)=255;
 	  *(greenFramePos+1)=255;
+	  *(greenFramePos+2)=255;
 	}
 	else if(*(inFramePos+2)==255){
+	  *(redFramePos)=255;
+	  *(redFramePos+1)=255;
 	  *(redFramePos+2)=255;
 	}
 	inFramePos+=3;
@@ -68,46 +135,51 @@ namespace vision{
       }
     }
     std::cout << inFrame.depth() << ", " << inFrame.channels() << std::endl;
-    SimpleBlobDetector::Params params;
-    params.minDistBetweenBlobs = 5.0;
-    params.minArea = 10.0;
-    params.filterByArea= true;
-    SimpleBlobDetector blob_detector(params);
-    std::cout<<"created detector"<<std::endl;
-    vector<cv::KeyPoint> keypoints;
-    std::cout<<"declared keypoints"<<std::endl;
+    
     blocks.clear();
     double frameHeight=inFrame.rows;
     double frameWidth=inFrame.cols;
     int i=0;
-    blob_detector.detect(redFrame, keypoints);
-    for (int i=0; i<keypoints.size()&&i<10; i++){
-      double X = keypoints[i].pt.x; 
-      double Y = keypoints[i].pt.y;
-      //printf("red: %d, x:%f, y:%f\n",i,X,Y);
+   
+    vector<double> point= findPoint(redFrame);
+    if(point[0]!=-1){
+      double X = point[0]; 
+      double Y = point[1];
+      //printf("red: x:%f, y:%f\n",X,Y);
+      
+      for(int x=std::max(0.0,X-1);x<std::min(X+2,frameWidth);x++){
+	for(int y=std::max(0.0,Y-1);y<std::min(Y+2,frameWidth);y++){
+	      inFrame.at<Vec3b>(Point(x,y))=Vec3b(255,0,0);
+	  }
+      }
+
       double forwardDistance=height * tan(90-hViewAngle/2-angleDown+hViewAngle * (frameHeight-Y)/frameHeight);
       double leftDistance=forwardDistance * tan(30-60*X/frameWidth);
       Color color(red);
       std::complex<double> position(forwardDistance,leftDistance);
-      //std::cout<<"making block"<<std::endl;
-      
       Block block(position,color);
-      blocks.push_back(&block);
+      blocks.push_back(block);
       //std::cout<<"made block"<<std::endl;
-    }    
-    blob_detector.detect(greenFrame, keypoints);
-    for (int i=0; i<keypoints.size()&&i<10; i++){
-      double X = keypoints[i].pt.x; 
-      double Y = keypoints[i].pt.y;
+    }
+
+    point= findPoint(redFrame);
+    if(point[0]!=-1){
+      double X = point[0]; 
+      double Y = point[1];
       //printf("green: %d, x:%f, y:%f\n",i,X,Y);
+
+      for(int x=std::max(0.0,X-1);x<std::min(X+2,frameWidth);x++){
+	for(int y=std::max(0.0,Y-1);y<std::min(Y+2,frameWidth);y++){
+	      inFrame.at<Vec3b>(Point(x,y))=Vec3b(255,0,0);
+	  }
+      }
+
       double forwardDistance=height * tan(90-hViewAngle/2-angleDown+hViewAngle * (frameHeight-Y)/frameHeight);
       double leftDistance=forwardDistance*tan(30-60*X/frameWidth);
       Color color(red);
       std::complex<double> position(forwardDistance,leftDistance);
-      //delete blocks[block];
-      //std::cout<<"making block"<<std::endl;
       Block block(position,color);
-      blocks.push_back(&block);
+      blocks.push_back(block);
       //std::cout<<"made block"<<std::endl;
     }
     //std::cout<<"made all blocks"<<std::endl;    
@@ -137,7 +209,7 @@ namespace vision{
  
     void Cam::poll(){
         std::cout << 1 << std::endl;
-	VideoCapture cap(0);
+	VideoCapture cap(1);
 	assert(cap.isOpened());
 	//	namedWindow( "Display window", WINDOW_AUTOSIZE );
 	Mat testFrame;
@@ -180,7 +252,7 @@ namespace vision{
 	runner.join();
     }
 
-    std::vector<Block*> Cam::getBlocks(){
+    std::vector<Block> Cam::getBlocks(){
 	return blocks;
     }
 
